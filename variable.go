11cbe7596052562b8760f4bed01034be9e71a9a7
A B Shravan Krishna <77732894+abshravan@users.noreply.github.com>
2025-07-23 10:22:39 +0530

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,9 @@
+package main
+import ("fmt")
+
+func main() {
+  var x int= 5 //varable declaration
+  var y := 10  //automatic datatype assigning
+  var x,y,z :=1,45,55 //multi variable assignments in single line
+  fmt.Println("%d %d",x,y)
+}
